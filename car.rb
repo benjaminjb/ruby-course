fa5f7657352f80b08b3da8757bb481e36af9a74e
Benjamin Blattberg <benjamin.blattberg@gmail.com>
2014-08-28 11:53:26 -0500

    Finished exercise in Car class creation

--- car.rb
+++ car.rb
@@ -12,4 +12,19 @@ class Car
 end
 
 class BigRig < Car
+
+  def initialize(color)
+    super(color,18)
+  end
+
+  def honk
+    'BBBBBRRRRRRAAAAAWWWHHHHH'
+  end
+end
+
+
+class Motorcycle < Car
+  def initialize()
+    super('red',2)
+  end
 end
--- car_stats.rb
+++ car_stats.rb
@@ -1,3 +1,26 @@
 class CarStats
-  # TODO
+  @@color_counter_hash = Hash.new(0)
+  @@sorted_popularity_key_array = []
+
+  def self.calc_top_color(cars)
+    self.color_counter_hash_builder(cars)  
+    @@color_counter_hash.key(@@sorted_popularity_key_array.last)
+  end
+
+  def self.calc_bottom_color(cars)
+    self.color_counter_hash_builder(cars)
+    @@color_counter_hash.key(@@sorted_popularity_key_array[0])
+  end
+
+  def self.color_counter_hash_builder(cars)
+    @@color_counter_hash.clear
+    cars.each { |car| @@color_counter_hash[car.color] += 1 }
+    @@sorted_popularity_key_array = @@color_counter_hash.values.sort
+  end
+
+  # instead of line 18, see min_by and max_by
+  # hash = {:a=>1,:b=>2,:c=>3}
+  # hash.min_by {|key,val| val} #=> [:a,1]
+  # hash.max_by {|key,val| val} #=> [:c,3]
+
 end
